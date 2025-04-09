395d6052c79c91741002d2e53e1cbf53020be318
Nao <32648183+naorovel@users.noreply.github.com>
2025-04-08 23:40:08 -0600

    Revert "Merge pull request #1 from naorovel/tableChanges"
    This reverts commit 8572eaa9c9f15d7f6fe298ba7b2b6b12e7d66dc3, reversing
    changes made to cab20caa25b9983b73e17f8f0b69300864592492.

--- api/index.py
+++ api/index.py
@@ -1,4 +1,3 @@
-
 from fastapi import FastAPI, UploadFile, HTTPException
 from fastapi.responses import StreamingResponse
 from fastapi.middleware.cors import CORSMiddleware
@@ -6,11 +5,6 @@ from pydantic import BaseModel
 import pandas as pd
 from json import loads, dumps
 import random
-
-#import sys
-#import os
-#sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-from api.detection import run_detection_and_return_table
 
 app = FastAPI()
 
@@ -73,7 +67,6 @@ def get_nodes_links_from_df(transactions_df):
     links = transactions_df["link"].values.tolist()
     return nodes, links
 
-
 @app.get("/get_table_transactions")
 async def get_table_transactions(): 
     global table_df, unique_addr
@@ -90,7 +83,6 @@ async def get_table_transactions():
     
     return parsed
 
-
 @app.get("/get_graph_transactions")
 async def get_graph_transactions(): 
     global graph_df
@@ -98,22 +90,6 @@ async def get_graph_transactions():
     print(len(links))
     print(len(nodes))
     return {'nodes': nodes, 'links': links}
-
-
-@app.get("/get_detected_transactions")
-async def get_detected_transactions(num_transactions:  int = 1000): 
-    detected_df = run_detection_and_return_table()
-    display_df = detected_df.head(num_transactions)
-
-    # Keep relevant columns
-    table_data = display_df[["source", "target", "type", "covert_generated", "is_covert"]]
-
-    # Convert to JSON and return
-    result = table_data.to_json(orient="records")
-    parsed = loads(result)
-    return parsed
-
-
 
 
 
